737858b47eb6d4cd063957b6202c0af990c27aa6
davidivad96 <david@strivelabs.io>
2022-07-16 18:20:35 +0200

    Initialize Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:mygymbro/constants.dart';
 import 'package:mygymbro/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
